ef2d54e483464b205461e7bf45c571a00013b120
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-10-24 20:42:59 +0530

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import About from "./components/AboutMe"
+import Gallery from "./components/Gallery"
 
 function App() {
 
   return (
     <>
-      <h2 className="text-center text-5xl text-red-500">frontend assignment</h2>
+      {/* <h2 className="text-center text-5xl text-red-500">frontend assignment</h2> */}
+      <div className="container mx-auto">
+        <div className="flex justify-center">
+          <div className="w-1/2"> first components</div>
+          <div className="w-1/2"> 
+           <About/>
+           <Gallery/>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
